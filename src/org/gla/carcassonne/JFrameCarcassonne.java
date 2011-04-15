d599dbb886a85e28e344714bb591f563376f9f37
Jean Vilver <jean.vilver@gmail.com>
2011-04-15 18:43:03 +0200

    [Rev 9] JPanel principal

--- src/org/gla/carcassonne/JFrameCarcassonne.java
+++ src/org/gla/carcassonne/JFrameCarcassonne.java
@@ -2,10 +2,13 @@ package org.gla.carcassonne;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Image;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.ImageIcon;
 import java.awt.FlowLayout;
 import org.gla.carcassonne.CarcassonneView;
+import org.gla.carcassonne.JPanelMain;
 import org.gla.carcassonne.CarcassonneController;
 
 public class JFrameCarcassonne extends CarcassonneView 
@@ -13,6 +16,7 @@ public class JFrameCarcassonne extends CarcassonneView
 
 	private JFrame jframe;
 	private JPanel jpanel;
+	private Image imageInitial;
 	private final static String TITLE = "Carcassonne";
 	private final static int WIDTH = 450;
 	private final static int HEIGHT = 350;
@@ -20,9 +24,10 @@ public class JFrameCarcassonne extends CarcassonneView
 	public JFrameCarcassonne(CarcassonneController controller) {
 		super(controller);
 		jframe = new JFrame(TITLE);
-		jpanel = new JPanel();
+		jpanel = new JPanelMain();
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.getContentPane().setLayout(new FlowLayout());
+		jframe.getContentPane().add(jpanel);
 		jframe.pack();
 		jframe.setSize(WIDTH, HEIGHT);
 	}
---
+++ src/org/gla/carcassonne/JPanelMain.java
@@ -0,0 +1,24 @@
+package org.gla.carcassonne;
+
+import javax.swing.ImageIcon;
+import java.awt.Image;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Toolkit;
+import javax.swing.JPanel;
+
+public class JPanelMain extends JPanel {
+	
+	private Image imageInitial;
+	private final static String cheminImageInitial = "res/drawable/tile-x.jpg";
+	
+	public JPanelMain() {
+		imageInitial = Toolkit.getDefaultToolkit().getImage(cheminImageInitial);
+	}
+
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D)g;
+		g2d.drawImage(imageInitial, 0, 0, this);
+	}
+}
