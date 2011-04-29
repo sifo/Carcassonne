05066c3d25812cc98e66ad415fe92aefb7472fd6
Jean Vilver <jean.vilver@gmail.com>
2011-04-30 00:25:13 +0200

    [Rev 27] Merge

--- src/org/gla/carcassonne/TileManager.java
+++ src/org/gla/carcassonne/TileManager.java
@@ -1,11 +1,9 @@
 package org.gla.carcassonne;
 
-import org.gla.carcassonne.Tile;
-import org.gla.carcassonne.Board;
-
 public class TileManager {
         private int numberOfTileRemaining;
         private Board board;
+        //private Map<TileType, Integer> tiles;
         private Tile [] tilesRemaining = 
                 { new Tile(TileType.TILE_A), new Tile(TileType.TILE_A), 
 
@@ -69,12 +67,45 @@ public class TileManager {
 
                 new Tile(TileType.TILE_X) 
         };
-
+        /*
+        private Map<TileType, Integer> tilesRemaining;
+        private static final int[] tilesCount = new int[] {
+        2, // Tuile A
+        2, // Tuile B
+        2, // Tuile C
+        2, // Tuile D
+        2, // Tuile E
+        2, // Tuile F
+        2, // Tuile G
+        2, // Tuile H
+        2, // Tuile I
+        2, // Tuile J
+        2, // Tuile K
+        2, // Tuile L
+        2, // Tuile M
+        2, // Tuile N
+        2, // Tuile O
+        2, // Tuile P
+        2, // Tuile Q
+        2, // Tuile R
+        2, // Tuile S
+        2, // Tuile T
+        2, // Tuile U
+        2, // Tuile V
+        2, // Tuile W
+        2 // Tuile X
+        };*/
+        
         private final static int MAX_TILE_NUMBER = 72;
 
         public TileManager() {
                 numberOfTileRemaining = MAX_TILE_NUMBER;
                 board = new Board();
+                /*tiles = new EnumMap<TileType, Integer>(TileType.class);
+
+                for (TileType t : TileType.values()) {
+                tiles.put(t, tilesCount[i++]);
+                }*/
         }
 
         public void putFirstTileOnBoard() {
@@ -89,7 +120,13 @@ public class TileManager {
                 int tilePosition = (int)(Math.random() * (higher - lower)) + lower;
                 return tilePosition;
         }
-
+        /*
+        public Tile selectTileRandomly() {
+        	RandomGenerator<EnumMap<TileType, Integer>> generator = new RandomGenerator<EnumMap<TileType, Integer>>(TileType.class);
+        	TileType t = generator.random();
+        	return new Tile(t);
+        }*/
+        
         public void removeTileRemaining(int tilePosition) {
                 int lastTile = numberOfTileRemaining - 1; 
                 tilesRemaining[tilePosition] = tilesRemaining[lastTile];
