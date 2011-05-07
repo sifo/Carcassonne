3d4717c3ae36de43980f8d53a7c9753f1d526fca
Jean Vilver <jean.vilver@gmail.com>
2011-05-07 22:50:29 +0200

    [Rev 32] Ajout d'une première version du client

---
+++ src/org/gla/carcassonne/network/CarcassonneClient.java
@@ -0,0 +1,13 @@
+package org.gla.carcassonne.network;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class CarcassonneClient extends Socket {
+
+	public CarcassonneClient(String host, int port)
+			throws UnknownHostException, IOException {
+		super(host, port);
+	}
+}
